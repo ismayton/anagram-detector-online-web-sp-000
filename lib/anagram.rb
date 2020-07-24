0f8e54ce161ffa40b8feb012d81bf1e47caaa4b0
Ian Mayton <ian.mayton@gmail.com>
2020-07-24 23:03:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,20 @@
 # Your code goes here!
 class Anagram
   
-  attr_accessor :word, 
+  attr_accessor :word_array, 
   
   def initialize(word)
-    @word = word 
+    @word_array = word.split('').sort 
   end
   
   def match(array)
-    
+    array.each do |word|
+      word_array = word.split('').sort
+      if @word_array = word_array
+        return word 
+      end
+    end
+    false
   end
+  
 end
