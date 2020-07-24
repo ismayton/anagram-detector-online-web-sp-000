7fad3d6ad755ffd608d651e17cdde508a4fbddcb
Ian Mayton <ian.mayton@gmail.com>
2020-07-24 23:13:01 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,11 +2,16 @@ require 'pry'
 
 class Anagram
   
+  attr_accessor :word
+  
   def initialize(word)
-    @word = word 
+    @word = word.split('').sort 
   end
   
   def match(array)
+    array.each do |word|
+      test_array = word.split('').sort
+    end
   end
  
   
